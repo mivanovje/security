a9da39cd014c009a6e3d5124f775d42054e5c7ab
Max Ivanov <mivanovje@gmail.com>
2021-09-12 19:24:26 +0300

    Initial commit

---
+++ src/test/java/mgivanov/qsmsa/security/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package mgivanov.qsmsa.security.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
